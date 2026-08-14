d4c20942ead24328557e9015ed8f2d8dddda8b13
code yuva <pandeyutk239@gmail.com>
2026-08-14 22:59:14 +0530

    fix backend api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://zyvo-backend-409q.onrender.com/api",
+  baseURL: "https://zyvo-backend-409g.onrender.com/api",
 });
 
 export default api;
